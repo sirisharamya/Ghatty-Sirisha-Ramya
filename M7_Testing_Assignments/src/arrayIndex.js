5f8e5dabd09e46ee8344ab6f20a819faa465dbb0
sirisharamya <108569087+sirisharamya@users.noreply.github.com>
2025-01-18 10:17:37 +0530

    Delete M7_Testing_Assignments directory

--- M7_Testing_Assignments/src/arrayIndex.js
+++
@@ -1,9 +0,0 @@
-function getElement(arr, index) {
-    if (index < 0 || index >= arr.length) {
-      throw new Error("Index out of bounds");
-    }
-    return arr[index];
-  }
-  
-  module.exports = { getElement };
-  
--- M7_Testing_Assignments/src/notificationService.js
+++
@@ -1,7 +0,0 @@
-function sendNotification(notificationService, message) {
-    const status = notificationService.send(message);
-    return status ? "Notification Sent" : "Failed to Send";
-  }
-  
-  module.exports = { sendNotification };
-  
